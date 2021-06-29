e452a7a53274e65e88a5dd6704d80ad622e4e09d
fed3n <fdemarchi95@gmail.com>
2021-06-29 14:07:21 +0200

    gpl license files

--- revdb.c
+++ revdb.c
@@ -1,4 +1,26 @@
 /*   
+ *   vdedns: proxy dns for resolution of hash based IPv6 addresses
+ *   
+ *   Copyright 2021 Federico De Marchi - Virtual Square Team 
+ *   University of Bologna - Italy
+ *   
+ *   This file is part of vdedns.
+ *
+ *   vdedns is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   vdedns is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
  *   hashdns.c: HASH based DNS
  *   revdb: data structure to hold data for reverse resolution
  *   
@@ -20,6 +42,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
